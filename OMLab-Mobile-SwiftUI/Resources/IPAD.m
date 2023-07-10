9bd70b91ae204ca9143e75e694c3558bcf3f6cc6
Christopher Bain <56360809+cbain00@users.noreply.github.com>
2023-07-10 12:15:05 -0400

    Add files via upload

--- OMLab-Mobile-SwiftUI/Resources/IPAD.m
+++ OMLab-Mobile-SwiftUI/Resources/IPAD.m
@@ -2,13 +2,13 @@
 %
 % Summary:
 % This class represents a UDP object that facilitates communication with a 
-% device using UDP (User Datagram Protocol). It provides methods for 
-% establishing a connection, setting the session name, starting and 
+% device using a UDP connection (User Datagram Protocol). It provides methods for
+% establishing a connection, setting the session name, starting and
 % stopping recording, sending event messages, and closing the connection.
 % 
-% The IPAD class is designed to interact with a device running an eye tracking
-% application. It handles the UDP communication necessary for sending commands
-% and receiving data from the device. The class supports functionalities such
+% The IPAD class is designed to interact with a device running OM-Lab Mobile.
+% It handles the UDP communication necessary for sending commands and receiving 
+% data from the device. The class supports functionalities such
 % as connecting to the device, setting the session name, initiating recording,
 % stopping recording, sending custom event messages, and closing the
 % connection.
@@ -34,6 +34,7 @@
 %   % Close the connection
 %   ipad.Close();
 
+
 classdef IPAD  < handle    
     properties
         ipAdd
@@ -43,49 +44,86 @@ classdef IPAD  < handle
     end
     
     methods
+        % Establish connection to UDP client
         function Connect(this, ip)
             this.ipAdd = ip;
             this.port = 8000;
             this.u = udpport;
     
-        % Attempt to establish the connection
             try
+                % Attempt to establish the connection
                 fopen(this.u);                
-                % Start connection status monitoring
-                this.StartConnectionStatusMonitoring();
-
                 fprintf('Connection established successfully.\n');
 
+                % Start connection status monitoring
+                fprintf('Beginning connection monitoring...\n')
+                this.StartConnectionStatusMonitoring();
+
             catch
-                fprintf('Failed to establish connection.\n');
+                fprintf('\nFailed to establish connection.\n');
             end
         end
 
+
+        % set name for recording session
         function SetSessionName(this, sessionName)
          %  write(this.u,['ChangeSetting?settingName=SessionName&value=' sessionName],"uint8",this.ipAdd,this.port)
         end
-        
+
+
         function StartRecording(this)
+           % Start recording through UDP client
            disp('Starting Recording...')
-           write(this.u,'StartRecording',"uint8",this.ipAdd,this.port);
-           data = read(this.u,this.u.NumBytesAvailable,"uint8");
-           disp(data)
-        end
-        
-        function StopRecording(this)
-           disp('Stopping Recording...')
-           write(this.u,'StopRecording',"uint8",this.ipAdd,this.port);
-        end
-        
-        function RecordEvent(this, message)
-           write(this.u,message,"uint8",this.ipAdd,this.port);
-        end  
-        
-        function Close(this)
-           this.u.delete
-           disp('Connection disconnected successfully.')
+           response = this.sendMessage('StartRecording');
+           disp(response)
         end
 
+        
+        function StopRecording(this)
+           % Stop recording
+           disp('Stopping Recording...')
+           response = this.sendMessage('StopRecording');
+           disp(response)
+        end
+
+        
+        % Record occurring event during session
+        function RecordEvent(this, message)
+           response = this.sendMessage(message);
+           disp(response)
+        end
+
+
+        % sending function for udp validation
+        function response = sendMessage(this, message)
+           tic;
+           write(this.u, message, "uint8", this.ipAdd,this.port);
+           while (this.u.NumBytesAvailable == 0)
+               seconds = toc;
+               if (seconds >= 0.5)
+                   error('Connection timeout.')
+               end
+           end
+           data = read(this.u,this.u.NumBytesAvailable,"uint8");
+           response = char(data);
+        end
+
+        
+        % close connection to client
+        function Close(this)
+            if isempty(this.u) || ~isvalid(this.u) || strcmp(this.u.Status, 'closed')
+                disp('Invalid: No connection to disconnect.')
+                return;
+            end
+            
+            this.StopConnectionStatusMonitoring();
+            this.u.delete;
+            disp('Connection disconnected successfully.');
+        end
+
+        
+        % start timer to let user know if connection has been dropped
+        % during session
         function StartConnectionStatusMonitoring(this)
             % Create a timer to periodically check the connection status
             this.connectionStatusTimer = timer('ExecutionMode', 'fixedRate', ...
@@ -95,7 +133,10 @@ classdef IPAD  < handle
             % Start the timer
             start(this.connectionStatusTimer);
         end
+
         
+        % if connection has dropped, print message to console
+        % (helper function for StartConnectionStatusMonitoring())
         function CheckConnectionStatus(this, ~, ~)
             if ~strcmp(this.u.Status, 'open')
                 fprintf('Connection lost.\n');
@@ -105,6 +146,9 @@ classdef IPAD  < handle
             end
         end
         
+        
+        % delete connection timer (helper function for 
+        % StartConnectionStatusMonitoring())
         function StopConnectionStatusMonitoring(this)
             % Stop and delete the connection status timer
             if ~isempty(this.connectionStatusTimer)
@@ -120,6 +164,3 @@ classdef IPAD  < handle
     end
     
 end
-
-
-
